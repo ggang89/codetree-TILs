6c944caf2a6bebd9d403fee1d23b19a7722d2c44
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-05-18 15:15:13 +0900

    Title: 간단한 사칙연산, Time: 213ms, Memory: 15MB, Status: Passed - CodeTree

--- 240518/간단한 사칙연산/simple-arithmetic-operation.js
+++ 240518/간단한 사칙연산/simple-arithmetic-operation.js
@@ -1,6 +1,8 @@
-let a=2;
-let b=5;
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
 
+let a = Number(input[0]);
+let b = Number(input[1]);
 console.log(a+b);
 console.log(a-b);
 console.log(parseInt(a/b));
