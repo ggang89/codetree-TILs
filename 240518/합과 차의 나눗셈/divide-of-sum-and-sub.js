5f0f09488865d9b3d068610f6ab288a9d2195664
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-05-18 15:48:29 +0900

    Title: 합과 차의 나눗셈, Time: 165ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240518/합과 차의 나눗셈/divide-of-sum-and-sub.js
@@ -0,0 +1,9 @@
+const fs=require("fs");
+
+let input=fs.readFileSync(0).toString().split(" ");
+
+let a=Number(input[0]);
+let b=Number(input[1]);
+let c=(a+b);
+let d=(a-b);
+console.log((c/d).toFixed(2));
