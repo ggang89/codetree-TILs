b52232261300fcb8deeeb1bd4aa06861bdf1b558
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-05-21 16:01:11 +0900

    Title: 삼항연산자, Time: 162ms, Memory: 14MB, Status: Passed - CodeTree

--- 240521/삼항연산자/ternary-operator.js
+++ 240521/삼항연산자/ternary-operator.js
@@ -1,5 +1,5 @@
 const fs=require("fs");
 
 let score=Number(fs.readFileSync(0).toString().trim());
-score=100? "pass":"failure";
-console.log(score);
+let result=score===100? "pass":"failure";
+console.log(result);
