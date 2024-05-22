71ee0c401e9663a7d8d60d1ad9489b74fb85ed57
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-05-22 15:14:48 +0900

    Title: 출석 부르기, Time: 183ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240522/출석 부르기/calling-attendance.js
+++ 240522/출석 부르기/calling-attendance.js
@@ -2,10 +2,10 @@ const fs=require("fs");
 
 let n = Number(fs.readFileSync(0).toString().trim());
 
-if(n=1){
+if(n===1){
     console.log("John")
-}else if(n=2){
+}else if(n===2){
     console.log("Tom")
-}else if(n=3){
+}else if(n===3){
     console.log("Paul");
 }
