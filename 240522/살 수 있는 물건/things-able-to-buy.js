7c99d19a47251706d858b589c1793b7442e74de0
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-05-22 15:04:32 +0900

    Title: 살 수 있는 물건, Time: 169ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240522/살 수 있는 물건/things-able-to-buy.js
@@ -0,0 +1,11 @@
+const fs =require("fs");
+
+let n = Number(fs.readFileSync(0).toString());
+
+if(n>=3000){
+console.log("book")
+}else if(3000>n>=1000){
+    console.log("mask")
+}else{
+    console.log("no");
+}
