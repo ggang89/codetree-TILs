5400b28329d017206c0a64c16a37aa6e7aa3edc7
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-05-24 19:09:23 +0900

    Title: 비교 연산, Time: 191ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240524/비교 연산/comparison-operator.js
@@ -0,0 +1,13 @@
+const fs =require("fs");
+
+let input=fs.readFileSync(0).toString().split(" ");
+
+let a=Number(input[0]);
+let b=Number(input[1]);
+
+console.log(a>=b? 1:0);
+console.log(a>b? 1:0);
+console.log(b>=a? 1:0);
+console.log(b>a? 1:0);
+console.log(a==b? 1:0);
+console.log(a != b? 1:0);
