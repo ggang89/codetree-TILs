0c60c2d8de5667e8a8754768ddb4f7372826bfcd
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-05-24 18:57:31 +0900

    Title: 3 또는 5의 배수, Time: 198ms, Memory: 47MB, Status: Passed - CodeTree

--- 240524/3 또는 5의 배수/multiples-of-3-or-5.js
+++ 240524/3 또는 5의 배수/multiples-of-3-or-5.js
@@ -5,7 +5,7 @@ let a=Number(fs.readFileSync(0).toString().trim());
 if(a%3 === 0){
     console.log("YES");
 }else{
-    console.log("No");
+    console.log("NO");
 }
 if(a%5 === 0){
     console.log("YES");
