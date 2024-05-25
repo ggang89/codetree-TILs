bb8aa3fa370b4e93f918a5a68597d28b3daf3308
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-05-25 13:44:35 +0900

    Title: 세 정수의 최솟값, Time: 237ms, Memory: 14MB, Status: Passed - CodeTree

--- 240525/세 정수의 최솟값/minimum-of-three-numbers.js
+++ 240525/세 정수의 최솟값/minimum-of-three-numbers.js
@@ -12,4 +12,10 @@ if(a<b && a<c){
     console.log(b);
 }else if(c<a && c<b){
     console.log(c);
+}else if(a<=b && b<=c){
+    console.log(a);
+}else if(b<=a && b<=c){
+    console.log(b);
+}else if(c<=a && c<= b){
+    console.log(c);
 }
