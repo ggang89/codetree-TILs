d037333587c96e09729dbebde541ec5fff6ec7d4
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-05-27 16:33:11 +0900

    Title: 장학금, Time: 182ms, Memory: 47MB, Status: Wrong Answer - CodeTree

--- 240527/장학금/scholarship.js
+++ 240527/장학금/scholarship.js
@@ -1,6 +1,6 @@
 const fs=require("fs");
 
-let n =fs.readFileSync(0).toString().split("\n");
+let input=fs.readFileSync(0).toString().split("\n");
 
 let middleScore = Number(input[0]);
 let finalScore = Number(input[1]);
