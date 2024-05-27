d0be35f858960802a9ec7386becdd824aaed5826
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-05-27 16:32:11 +0900

    Title: 장학금, Time: 183ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240527/장학금/scholarship.js
+++ 240527/장학금/scholarship.js
@@ -5,10 +5,10 @@ let n =fs.readFileSync(0).toString().split("\n");
 let n1=Number(n[0]);
 let n2=Number(n[1]);
 
-if(n1 >= 90 && n2>=95){
+if(n1 >= 90 && n2 >= 95){
     console.log(100000);
-}else if(n1>=90 && n2 >= 90){
+}else if(n1 >= 90 && n2 >= 90){
     console.log(50000);
-}else{
+}else {
     console.log(0);
-}
+};
