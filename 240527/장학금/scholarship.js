c367686c0817d655dd2b1083b9a29954d2601515
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-05-27 16:32:40 +0900

    Title: 장학금, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240527/장학금/scholarship.js
+++ 240527/장학금/scholarship.js
@@ -2,13 +2,16 @@ const fs=require("fs");
 
 let n =fs.readFileSync(0).toString().split("\n");
 
-let n1=Number(n[0]);
-let n2=Number(n[1]);
+let middleScore = Number(input[0]);
+let finalScore = Number(input[1]);
 
-if(n1 >= 90 && n2 >= 95){
+// 출력
+if (middleScore >= 90 && finalScore >= 95) {
     console.log(100000);
-}else if(n1 >= 90 && n2 >= 90){
-    console.log(50000);
-}else {
+}
+else if (middleScore >= 90 && finalScore >= 90) {
+    console.log(50000)
+}
+else {
     console.log(0);
-};
+}
