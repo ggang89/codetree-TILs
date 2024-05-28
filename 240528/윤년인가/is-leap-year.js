3909f08948ea5491a5f71189a90e778637207762
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-05-28 17:24:10 +0900

    Title: 윤년인가, Time: 181ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 240528/윤년인가/is-leap-year.js
+++ 240528/윤년인가/is-leap-year.js
@@ -4,8 +4,8 @@ let y=Number(fs.readFileSync(0).toString().trim());
 
 if( y % 4 === 0 ){
     console.log("true");
-}else  if(y % 100 === 0 ){
-    if(y % 400 === 0){
+}else {
+    if(y % 100 === 0 && y % 400 === 0){
         console.log("true")
     }else{
         console.log("false")
