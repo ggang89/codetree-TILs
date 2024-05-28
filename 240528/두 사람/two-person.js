11e8665b49e6bac903f81762ba1b1a0641aeb4a9
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-05-28 16:49:56 +0900

    Title: 두 사람, Time: 218ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240528/두 사람/two-person.js
@@ -0,0 +1,19 @@
+const fs=require("fs");
+
+let input=fs.readFileSync(0).toString().split("\n");//[ '20 M', '20 W' ]
+
+let input1=input[0].split(" ");//[ '20', 'M' ]
+
+let input2=input[1].split(" ");//['20', 'w' ]
+
+let s1_year=Number(input1[0]);
+let s1_gender=input1[1];
+
+let s2_year=Number(input2[0]);
+let s2_gender=input2[1];
+
+if(s1_gender === 'M' || s2_gender === 'M'){
+    console.log(1);
+}else{
+    console.log(0);
+}
