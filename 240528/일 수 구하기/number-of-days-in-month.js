d93f4e2145508d79056f003c7359142a48b9b426
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-05-28 18:03:52 +0900

    Title: 일 수 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240528/일 수 구하기/number-of-days-in-month.js
+++ 240528/일 수 구하기/number-of-days-in-month.js
@@ -8,20 +8,22 @@ let y=Number(fs.readFileSync(0).toString().trim());
 //3. 7보다 작거나 같은 달은 홀수달이 31,짝수달은 30일이고
 //   7보다 큰 달은 홀수달이 30, 짝수달이 31일이다
 
-if(y !== 2){
-    if(y <= 7){
-        if(y % 2 === 0){
-            console.log(30)
-        }else{
-            console.log(31)
-        }
-    }else if(y >8){
-        if(y % 2 ===0){
-            console.log(31)
-        }else{
-            console.log(30)
-        }
-    }
-}else{
+if (n === 2) {
     console.log(28);
+}
+else if (n <= 7) {
+    if (n % 2 === 1) {
+        console.log(31);
+    }
+    else {
+        console.log(30);
+    }
+}
+else {
+    if (n % 2 === 0) {
+        console.log(31);
+    }
+    else {
+        console.log(30);
+    }
 }
