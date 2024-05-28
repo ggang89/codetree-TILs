d72c9d84b0a4e2a69151535aba8a67b459ccb2f1
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-05-28 16:23:56 +0900

    Title: 좀 더 어려운 수학 점수, Time: 176ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240528/좀 더 어려운 수학 점수/math-scores-are-more-difficult.js
@@ -0,0 +1,26 @@
+const fs=require("fs");
+
+let input=fs.readFileSync(0).toString().split("\n");
+
+let a=input[0];//80 90
+let b=input[1];//90 80
+
+let aa=a.split(" ");//['80','90']
+let bb=b.split(" ");
+
+let aMath = Number(aa[0]);
+let aEng = Number(aa[1]);
+
+let bMath=Number(bb[0]);
+let bEng=Number(bb[1]);
+
+if(aMath > bMath){
+    console.log("A");
+}else{
+    console.log("B");
+}
+if(aMath === bMath && aEng > bEng){
+    console.log("A")
+}else if(aMath === bMath && bEng > aEng){
+    console.log("B");
+}
