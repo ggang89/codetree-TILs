01af42a00473fc5ad041651589a3082dbc9ea420
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-05-28 18:02:56 +0900

    Title: 일 수 구하기, Time: 181ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240528/일 수 구하기/number-of-days-in-month.js
@@ -0,0 +1,27 @@
+const fs=require("fs");
+
+let y=Number(fs.readFileSync(0).toString().trim());
+
+//1. 먼저 2월인지 판단한다. 2월이면 28일 출력
+//2. 2월이 아니면 7보다 같거나 작은지, 7보다 큰지 판단한다
+//(7월에서 8월로 넘어가는 기점에 홀수달과 짝수달의 일수가 달라짐)
+//3. 7보다 작거나 같은 달은 홀수달이 31,짝수달은 30일이고
+//   7보다 큰 달은 홀수달이 30, 짝수달이 31일이다
+
+if(y !== 2){
+    if(y <= 7){
+        if(y % 2 === 0){
+            console.log(30)
+        }else{
+            console.log(31)
+        }
+    }else if(y >8){
+        if(y % 2 ===0){
+            console.log(31)
+        }else{
+            console.log(30)
+        }
+    }
+}else{
+    console.log(28);
+}
