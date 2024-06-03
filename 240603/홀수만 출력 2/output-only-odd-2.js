8fa2cbe59ff6bf0d00e16bb4db69bde92af5bb18
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-06-03 22:56:25 +0900

    Title: 홀수만 출력 2, Time: 183ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240603/홀수만 출력 2/output-only-odd-2.js
@@ -0,0 +1,13 @@
+const fs=require("fs");
+
+let input=fs.readFileSync(0).toString().split(" ");
+
+let b=Number(input[0]);
+let a=Number(input[1]);
+
+let result="";
+
+for(let i=b; i>=a; i=i-2){
+     result=result+i+" ";
+}
+console.log(result);
