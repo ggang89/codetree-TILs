d2135b530bb649c674b848e15c2603ff7a9ac939
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-06-03 23:32:00 +0900

    Title: n까지 숫자 출력, Time: 161ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240603/n까지 숫자 출력/print-1-to-n.js
@@ -0,0 +1,11 @@
+const fs=require("fs");
+
+let n=Number(fs.readFileSync(0).toString().trim());
+
+let i=1;
+let result="";
+
+while(i<=n){
+    result=result+i+" ";
+    i++
+}; console.log(result);
