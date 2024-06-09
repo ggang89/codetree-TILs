44a9d762a39050400d0a533dd642eccd7419eb69
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-06-09 21:51:44 +0900

    Title: 숫자의 증감, Time: 1512ms, Memory: 160MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240609/숫자의 증감/increasing-and-decreasing-numbers.js
@@ -0,0 +1,19 @@
+const fs =require("fs");
+
+let input = fs.readFileSync(0).toString().split(" ");
+
+let c = input[0];
+let n = Number(input[1]);
+let result="";
+if(c === "A"){
+    for(i = 1; i <= n; i++){
+        result +=i+" "
+       
+    } console.log(result);
+}
+ else if(c === "D"){
+    for(i = n; i =1; i--){
+        result +=n;
+        
+    }console.log(result);
+}
