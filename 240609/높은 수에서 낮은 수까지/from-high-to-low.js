e82bf490bf2debbf390283e10b23e0e2edb9d1a1
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-06-09 21:58:20 +0900

    Title: 높은 수에서 낮은 수까지, Time: 186ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240609/높은 수에서 낮은 수까지/from-high-to-low.js
@@ -0,0 +1,18 @@
+const fs=require("fs");
+let input =fs.readFileSync(0).toString().split(" ");
+
+let a=Number(input[0]);
+let b=Number(input[1]);
+
+let result="";
+
+if(a > b){
+    for(i = a; i >= b; i-- ){
+        result += i+" ";
+    }
+}else{
+    for(i = b; i >= a; i--){
+        result +=i+ " ";
+    }
+}
+console.log(result);
