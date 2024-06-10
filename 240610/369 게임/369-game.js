95b11cbb0acb31418c520d56241b9720d38365c6
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-06-10 21:15:41 +0900

    Title: 369 게임, Time: 234ms, Memory: 14MB, Status: Passed - CodeTree

--- 240610/369 게임/369-game.js
+++ 240610/369 게임/369-game.js
@@ -1,12 +1,22 @@
-const fs=require("fs");
+// 변수 선언 및 입력
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
 
-let n =Number(fs.readFileSync(0).toString());
-let result="";
-for(i = 1 ; i <= n ; i++){
-    if(i % 3 === 0){
-        result +=0+" ";
-    }else{
-        result +=i+" ";
+// 출력
+let result = "";
+for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0) {
+        result += "0 ";
+    }
+    else if (i % 10 === 3 || i % 10 === 6 || i % 10 === 9) {
+        result += "0 ";
+    }
+    else if (parseInt(i / 10) === 3 || parseInt(i / 10) === 6 || parseInt(i / 10) === 9) {
+        result += "0 ";
+    }
+    else {
+        result += i + " ";
     }
 }
+
 console.log(result);
