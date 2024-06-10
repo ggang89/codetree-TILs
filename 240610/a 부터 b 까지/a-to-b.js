3a6c2bf01a9d983baf2c18eee108db92d845583c
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-06-10 19:43:31 +0900

    Title: a 부터 b 까지, Time: 221ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240610/a 부터 b 까지/a-to-b.js
@@ -0,0 +1,23 @@
+const fs=require("fs");
+
+let input =fs.readFileSync(0).toString().split(" ");
+
+let a=Number(input[0]);
+let b=Number(input[1]);
+
+let result=a;
+
+for(i = a; i < b; ){
+    
+        if(i % 2 === 1){
+        i=i*2;
+        result +=" "+i;
+        
+        }else if(i % 2 === 0){
+         i=i+3;
+        result += " " +i;
+    }
+    
+    
+};
+console.log(result);
