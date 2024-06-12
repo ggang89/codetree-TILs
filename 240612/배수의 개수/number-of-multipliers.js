aaccb4758a253ad68073acd443914e1d475623a2
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-06-12 19:07:19 +0900

    Title: 배수의 개수, Time: 188ms, Memory: 14MB, Status: Passed - CodeTree

--- 240612/배수의 개수/number-of-multipliers.js
+++ 240612/배수의 개수/number-of-multipliers.js
@@ -2,16 +2,32 @@ const fs = require("fs");
 
 let input = fs.readFileSync(0).toString().split("\n");
 
-let cnt_3 = 0;
-let cnt_5 = 0;
+// let cnt_3 = 0;
+// let cnt_5 = 0;
 
-for(let i = 0; i < input.length; i++){
-    let n = Number(input[i]); //input요소 하나하나를 숫자로 변환
-    if(n % 3 === 0){
-        cnt_3++;
+// for(let i = 0; i < input.length; i++){
+//     let n = Number(input[i]); //input요소 하나하나를 숫자로 변환
+//     if(n % 3 === 0){
+//         cnt_3++;
+//     }
+//      if(n % 5 === 0){
+//         cnt_5++;
+//     }
+// };
+// console.log(cnt_3,cnt_5);
+
+let cntA = 0, cntB = 0;
+
+for (let i = 0; i < 10; i++) {
+    let x = Number(input[i]);
+    if (x % 3 === 0) {
+        cntA++;
     }
-     if(n % 5 === 0){
-        cnt_5++;
+
+    if (x % 5 === 0) {
+        cntB++;
     }
-};
-console.log(cnt_3,cnt_5);
+}
+
+// 출력
+console.log(cntA, cntB);
