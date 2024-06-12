b9591f79cb62eb3028b068bae41b8f7d921b68ab
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-06-12 21:51:25 +0900

    Title: 등장횟수 확인, Time: 306ms, Memory: 14MB, Status: Passed - CodeTree

--- 240612/등장횟수 확인/check-number-of-appearances.js
+++ 240612/등장횟수 확인/check-number-of-appearances.js
@@ -4,7 +4,7 @@ let input= fs.readFileSync(0).toString().split("\n");
 
 let cnt=0;
 
-for(let i=0; i< input.length; i++){
+for(let i=0; i < 5; i++){
     let x = Number(input[i]);
     if(x % 2 === 0){
         cnt++;
