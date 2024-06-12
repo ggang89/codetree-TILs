a897caad615471f55ed1d5bb3a191d13d3e6aa99
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-06-12 19:09:28 +0900

    Title: 배수의 개수, Time: 193ms, Memory: 14MB, Status: Passed - CodeTree

--- 240612/배수의 개수/number-of-multipliers.js
+++ 240612/배수의 개수/number-of-multipliers.js
@@ -2,32 +2,33 @@ const fs = require("fs");
 
 let input = fs.readFileSync(0).toString().split("\n");
 
-// let cnt_3 = 0;
-// let cnt_5 = 0;
+let cnt3 = 0;
+let cnt5 = 0;
 
-// for(let i = 0; i < input.length; i++){
-//     let n = Number(input[i]); //input요소 하나하나를 숫자로 변환
-//     if(n % 3 === 0){
-//         cnt_3++;
-//     }
-//      if(n % 5 === 0){
-//         cnt_5++;
-//     }
-// };
-// console.log(cnt_3,cnt_5);
-
-let cntA = 0, cntB = 0;
-
-for (let i = 0; i < 10; i++) {
-    let x = Number(input[i]);
-    if (x % 3 === 0) {
-        cntA++;
+for(let i = 0; i < 10; i++){
+    let n = Number(input[i]); //input요소 하나하나를 숫자로 변환
+    if(n % 3 === 0){
+        cnt3++;
     }
-
-    if (x % 5 === 0) {
-        cntB++;
+     if(n % 5 === 0){
+        cnt5++;
     }
-}
+};
+console.log(cnt3,cnt5);
 
-// 출력
-console.log(cntA, cntB);
+
+// let cntA = 0, cntB = 0;
+
+// for (let i = 0; i < 10; i++) {
+//     let x = Number(input[i]);
+//     if (x % 3 === 0) {
+//         cntA++;
+//     }
+
+//     if (x % 5 === 0) {
+//         cntB++;
+//     }
+// }
+
+// // 출력
+// console.log(cntA, cntB);
