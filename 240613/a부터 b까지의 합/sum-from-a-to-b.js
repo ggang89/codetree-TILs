ef01ec24b0a086fc5a15f8d4f3c0011261b2d579
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-06-13 19:25:36 +0900

    Title: a부터 b까지의 합, Time: 234ms, Memory: 14MB, Status: Passed - CodeTree

--- 240613/a부터 b까지의 합/sum-from-a-to-b.js
+++ 240613/a부터 b까지의 합/sum-from-a-to-b.js
@@ -6,8 +6,10 @@ let a = Number(input[0]);
 let b = Number(input[1]);
 
 let sum=a;
+//let sum=0으로 해줘야 함. ""으로 하면 문자열로 더해짐
 
 for(let i=a; i <=b; i++){
     sum +=i;
 }
 console.log(sum-a);
+//sum을 0으로 시작하면 a를 빼줄 필요 없다
