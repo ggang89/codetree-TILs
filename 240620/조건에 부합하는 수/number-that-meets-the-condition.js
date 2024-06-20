413661210135677f7a1e02ddf0839418dd411b16
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-06-20 18:28:09 +0900

    Title: 조건에 부합하는 수, Time: 308ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240620/조건에 부합하는 수/number-that-meets-the-condition.js
@@ -0,0 +1,20 @@
+const fs=require("fs");
+
+let a=Number(fs.readFileSync(0).toString().trim());
+
+let result="";
+
+for(let i = 1; i <= a; i++){
+
+if((i % 2 === 0 && i % 4 !== 0)){
+    continue;
+}
+if((Math.floor(i / 8))%2 === 0){
+    continue;
+}
+if((i % 7) < 4){
+    continue;
+}
+    result +=i+" ";
+}
+console.log(result);
