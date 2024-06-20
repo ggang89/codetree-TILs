eaaa2ae35e2755eafe6e1a1455bfa7063f126014
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-06-20 15:24:58 +0900

    Title: 20대의 평균나이, Time: 5074ms, Memory: 17MB, Status: Time Limit Exceed - CodeTree

---
+++ 240620/20대의 평균나이/average-age-of-20.js
@@ -0,0 +1,21 @@
+const fs=require("fs");
+
+let input = fs.readFileSync(0).toString().split("\n");
+let index=0;
+let sum=0;
+let cnt=0;
+while(true){
+    let old=Number(input[index]);
+    if(old < 30){
+    sum += old;
+    cnt++;
+    index++;
+    }
+    
+    if(old >= 30){
+        
+        break;
+    }
+
+}
+console.log((sum/cnt).toFixed(2));
