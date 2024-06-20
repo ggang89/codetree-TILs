a388f9b16aa3e4b132cb9c8fb982b6de3ed82f5e
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-06-20 18:01:59 +0900

    Title: 3n + 1 수열, Time: 162ms, Memory: 14MB, Status: Passed - CodeTree

--- 240620/3n + 1 수열/3n-plus-1-sequence.js
+++ 240620/3n + 1 수열/3n-plus-1-sequence.js
@@ -6,7 +6,7 @@ while(true){
     if(n % 2 === 0){
        n = n / 2;
        cnt++;
-    }else if(n % 2 === 1){
+    }else if(n % 2 === 1 && n !== 1){
         n = (n * 3) + 1;
         cnt++;
     }
