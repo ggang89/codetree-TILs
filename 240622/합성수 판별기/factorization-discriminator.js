b2ba84d87d110a4944092799a368dd409c742258
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-06-22 15:02:25 +0900

    Title: 합성수 판별기, Time: 166ms, Memory: 14MB, Status: Passed - CodeTree

--- 240622/합성수 판별기/factorization-discriminator.js
+++ 240622/합성수 판별기/factorization-discriminator.js
@@ -7,10 +7,9 @@ let x = false;
 for(i =2; i <=(n-1); i++){
     if(n % i === 0){
         x =true;
-       
     };
 }
-if(x=true){
+if(x ===true){
     console.log("C");
 }else{
     console.log("N");
