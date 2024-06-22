5f60e1238495a872a0354e62542e3be055202686
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-06-22 14:22:09 +0900

    Title: 2의 거듭제곱수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240622/2의 거듭제곱수/pow-of-2.js
@@ -0,0 +1,18 @@
+const fs= require("fs");
+
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+let cnt = 1;
+let prod = 2;
+
+while (true) {
+    // prod가 n이 될 때까지 2를 곱합니다.
+    if (prod === n) {
+        break;
+    }    
+    
+    prod *= 2;
+    cnt++;
+}
+
+console.log(cnt);
