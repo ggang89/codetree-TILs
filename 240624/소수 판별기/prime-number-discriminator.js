ee3ab0a24fe6ca1874d4156a69d40409b61ad24d
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-06-24 22:01:29 +0900

    Title: 소수 판별기, Time: 193ms, Memory: 14MB, Status: Passed - CodeTree

--- 240624/소수 판별기/prime-number-discriminator.js
+++ 240624/소수 판별기/prime-number-discriminator.js
@@ -4,9 +4,17 @@ let n=Number(fs.readFileSync(0).toString());
 
 let satisfied = true;
 
-if(n % n ===0 && n % 1 ===0){
-    satisfied = true;
-    console.log("P")
-}else{
-    console.log("C")
+for (let i = 2; i < n; i++) {
+    // a에서 b 사이의 값 중 c의 배수가 있는지 확인합니다.
+    if (n % i === 0) {
+        satisfied = false;
+    }
+}
+
+// 출력
+if (satisfied === true) {
+    console.log("P");
+}
+else {
+    console.log("C");
 }
