5463e32375301cf33c50fd563298102148ab1ae4
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-06-24 22:23:13 +0900

    Title: 직사각형 별표 출력하기, Time: 191ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240624/직사각형 별표 출력하기/print-stars-in-rectangle.js
@@ -0,0 +1,12 @@
+const fs=require("fs");
+
+let input = fs.readFileSync(0).toString().split(" ");
+let n =Number(input[0]);
+let m =Number(input[1]);
+let str="";
+for(let i=1; i<=n;i++){
+    str=""
+    for(let j=1; j <= m; j++){
+        str +="*"+" "
+    }console.log(str);
+}
