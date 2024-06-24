d3d3e570c70b74d75f5b8a2c2b0a89b7e94607c7
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-06-24 22:10:44 +0900

    Title: 모두 3의 배수, Time: 185ms, Memory: 14MB, Status: Passed - CodeTree

--- 240624/모두 3의 배수/all-multiples-of-3.js
+++ 240624/모두 3의 배수/all-multiples-of-3.js
@@ -1,13 +1,15 @@
 const fs=require("fs");
 
-let input=fs.readFileSync(0).toString();
+let input=fs.readFileSync(0).toString().trim();
+let arr=input.split("\n");
 
 let satisfied= true;
 
 for(let i=0;i<5;i++){
-    let n=Number(input[i]);
+    let n=Number(arr[i]);
     if( n % 3 !== 0){
         satisfied = false;
+        break;
     }
 }
 if(satisfied === true){
