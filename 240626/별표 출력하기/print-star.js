22d7853df8472afaf1abd866bb0d2575c6427940
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-06-26 15:06:54 +0900

    Title: 별표 출력하기, Time: 188ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240626/별표 출력하기/print-star.js
@@ -0,0 +1,20 @@
+const fs=require("fs");
+
+let n = Number(fs.readFileSync(0).toString().trim());
+
+let str="";
+
+for(let i =1; i <= n; i++){
+    str="";
+    for(let j = 1; j <= i; j++){
+        str +="* "
+    }
+    console.log(str)
+}
+for(let i = 1; i <= n-1; i++){
+    str="";
+    for(let j = 1; j <= n-i; j++){
+        str +="* "
+    }
+    console.log(str)
+}
