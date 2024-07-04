29ecd3fa25debab2dfbb448df8a8c7fc1de8c0e5
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-07-04 18:57:13 +0900

    Title: 별표 출력하기 13, Time: 294ms, Memory: 47MB, Status: Passed - CodeTree

--- 240704/별표 출력하기 13/print-start-13.js
+++ 240704/별표 출력하기 13/print-start-13.js
@@ -1,20 +1,19 @@
 const fs= require("fs")
 let n= Number(fs.readFileSync(0).toString().trim())
 let count = 1;
-
+const arr = [];
 for(let i=1; i <= n; i++){ //  1 2 3 
     if(i % 2 === 0){
-        console.log("* ".repeat(count));
+        arr.push("* ".repeat(count));
         count += 1;
     } else {
-        console.log("* ".repeat(n-count+1))
+        arr.push("* ".repeat(n-count+1))
     }
 }
-for(let i=1; i<=n; i++){
-    if(i % 2 === 0){
-        console.log("* ".repeat(count));
-    } else {
-        console.log("* ".repeat(n-count+1));
-        count -= 1;
-    }
+for(let s of arr){
+    console.log(s);
+}
+arr.reverse();
+for(let s of arr){
+    console.log(s);
 }
