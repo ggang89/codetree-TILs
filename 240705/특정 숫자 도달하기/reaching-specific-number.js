61083847855ff8e7ab788d0099be80ff301b2c70
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-07-05 13:20:05 +0900

    Title: 특정 숫자 도달하기, Time: 133ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 240705/특정 숫자 도달하기/reaching-specific-number.js
@@ -0,0 +1,23 @@
+/*let givenArr = [1, 3, 2, 5, 4, 6, 8, 1, 10];
+
+# 짝수만 추출합니다.
+let evenArr = [];
+for (let elem of givenArr) {
+    if (elem % 2 === 0) evenArr.push(elem);
+}
+
+# 짝수로만 이루어진 리스트
+console.log(evenArr);
+
+>> [2, 4, 6, 8, 10] */
+
+//2번째 숫자부터 끝까지의 합 구하기
+/*let arr = [1, 2, 2, 5];
+
+let sumVal = 0;
+for (let elem of arr.slice(1)) sumVal += elem;
+
+console.log(sumVal);
+
+>> 9
+*/
