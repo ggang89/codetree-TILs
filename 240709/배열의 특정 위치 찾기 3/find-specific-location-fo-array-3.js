0caefdccabf0dca1c58503c5cbb20d5dbd35f42d
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-07-09 16:27:29 +0900

    Title: 배열의 특정 위치 찾기 3, Time: 169ms, Memory: 47MB, Status: Passed - CodeTree

--- 240709/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.js
+++ 240709/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.js
@@ -2,9 +2,12 @@ const fs=require("fs")
 
 let input=fs.readFileSync(0).toString().trim().split(" ").map(Number);
 let sum=0
+let arr=[]
 
 for(let i=0; i < input.length; i++){
-    if(input[i] === 0) break;
-    sum +=(input[i])
+    if(input[i] === 0) {
+        k=i; //0이 되는 인덱스번호를 k에 저장
+        break;
+    }
 }
-console.log(sum)
+console.log(input[k-1]+input[k-2]+input[k-3]);
