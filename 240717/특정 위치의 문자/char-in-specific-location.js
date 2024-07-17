7d81b00a6ef3003bcdd9bbe8572734203aef6dd9
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-07-17 18:23:13 +0900

    Title: 특정 위치의 문자, Time: 268ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240717/특정 위치의 문자/char-in-specific-location.js
@@ -0,0 +1,16 @@
+const fs=require("fs")
+let input=fs.readFileSync(0).toString().trim();
+
+let words = [ 'L', 'E', 'B', 'R', 'O', 'S'];
+let idx=-1
+for(let i = 0; i < 6; i++){
+    if(words[i] === input){
+        idx=i;
+        
+    }
+}
+if (idx === -1) {
+    console.log("None");
+}
+else {
+    console.log(idx);}
