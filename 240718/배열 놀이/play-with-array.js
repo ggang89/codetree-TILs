c8e25de72775c0c4b186e43f558ccdf436f97ab7
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-07-18 19:21:10 +0900

    Title: 배열 놀이, Time: 175ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240718/배열 놀이/play-with-array.js
@@ -0,0 +1,18 @@
+const fs=require("fs");
+
+let [, tempArrString, ...rest] = fs.readFileSync(0).toString().split("\n");
+const arr = tempArrString.split(" ").map(Number);
+
+for(const r of rest){
+  if(r[0] === "1"){
+    const [, index] = r.split(" ").map(Number);
+    console.log(arr[index-1]);
+  } else if(r[0] === "2"){
+    const target = Number(r[2]);
+    const value = arr.findIndex(v => v === target);
+    console.log(value+1);
+  } else {
+    const [, s, e] = r.split(" ").map(Number);
+    console.log(arr.slice(s-1, e).join(" "))
+  }
+}
