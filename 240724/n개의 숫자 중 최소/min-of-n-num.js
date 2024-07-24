d505fb2287937b55181b95feff16edbd83834ac5
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-07-24 22:26:12 +0900

    Title: n개의 숫자 중 최소, Time: 160ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240724/n개의 숫자 중 최소/min-of-n-num.js
+++ 240724/n개의 숫자 중 최소/min-of-n-num.js
@@ -6,9 +6,9 @@ let arr=input[1].split(" ").map(Number)
 let min=arr[0]
 let cnt=1;
 for(const a of arr){
-    if(min>a){//지금까지 나왔던 값들 보다 더 작은 값이면
-         min=a; // 그 값을 최소값으로 갱신하고
-        cnt=1; //cnt는 1롤 초기화한다
+    if(min > a){//지금까지 나왔던 값들 보다 더 작은 값이면
+         min = a; // 그 값을 최소값으로 갱신하고
+        cnt = 1; //cnt는 1롤 초기화한다
     }else if(min === a){ //지금까지 나온 값과 같다면
         cnt++; //cnt을 1 증가시킨다.
     }
