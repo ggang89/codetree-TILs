524dfd2d54100c19942350193210f0a681f5e3c7
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-08-02 23:03:56 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 194ms, Memory: 15MB, Status: Passed - CodeTree

--- 240802/중복되지 않는 숫자 중 최대/max-of-unique-number.js
+++ 240802/중복되지 않는 숫자 중 최대/max-of-unique-number.js
@@ -3,18 +3,19 @@ let input=fs.readFileSync(0).toString().trim().split("\n");
 let n=Number(input[0]);
 let arr=input[1].split(" ").map(Number);//1 2 1
 //3231
-let max=arr[0]
-let cnt=0
-for(let i = 0; i < n; i++){
-    if(arr[i] > max){
-         max = arr[i]
-        cnt++;
-    }else if(arr[i]===max){
-        let arr2=arr.pop(arr[i]);
-        for(let j=0;j<arr2.length;j++){
-            if(arr2[j]>max) max=arr2[i]
+let max= -1;
+
+for(let a of arr){
+    if(max < a){
+        let cnt=0;
+        for(let a2 of arr){
+            if(a2===a){
+                cnt +=1;
+            }
+        }
+        if(cnt === 1){
+            max=a
         }
     }
-    
 }
 console.log(max)
