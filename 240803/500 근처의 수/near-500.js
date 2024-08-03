cb10686824790d64c13cc8071d4d96c5ecc3cabd
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-08-03 15:15:14 +0900

    Title: 500 근처의 수, Time: 204ms, Memory: 47MB, Status: Passed - CodeTree

--- 240803/500 근처의 수/near-500.js
+++ 240803/500 근처의 수/near-500.js
@@ -1,6 +1,6 @@
 const fs=require('fs');
 let input=fs.readFileSync(0).toString().trim().split(" ").map(Number);
-
+//951 122 379 790 286 468 931 724 1000 901 
 let mx=0
 let min
 let mxArr=[]
@@ -14,13 +14,19 @@ for(let i of input){
 for(let a of mxArr){
     if(a > mx) mx = a
 }
-
+//console.log("큰수",mx)
 for(let i of input){
-    if(i >500) minArr.push(i)  //console.log(minArr) [ 872, 887 ]
+    if(i >500) minArr.push(i)  
 }
-for(let a of minArr){
-    //console.log(minArr,a)
- if(a <= minArr[0]) min =a 
-}
-
- console.log(mx,min)
+//console.log(minArr) //[ 872, 887 ][ 951, 790, 931, 724, 1000, 901 ]
+// for(let a of minArr){
+//     //console.log(minArr,a)
+//     min=minArr[0] //951
+    
+//  if(a < min){ 
+//     console.log(a)
+//     min =a
+//  } 
+// }
+let c=minArr.sort((a,b)=>a-b);
+console.log(mx,c[0])
