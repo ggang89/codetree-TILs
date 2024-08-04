8f991c0a41ea443048983d2d40aaae83d20a5569
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-08-04 19:53:06 +0900

    Title: 두 숫자의 차의 최솟값, Time: 165ms, Memory: 14MB, Status: Passed - CodeTree

--- 240804/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.js
+++ 240804/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.js
@@ -2,10 +2,16 @@ const fs = require("fs")
 let input = fs.readFileSync(0).toString().trim().split("\n");
 let n =Number(input[0])
 let arr = input[1].split(" ").map(Number);
-let minValue = arr[0];
-
+let minValue ;
+let valueArr=[];
 for(let i = 0;i < n-1; i++){
     let value= arr[i+1]-arr[i]
-    if(value < minValue) minValue = value
+    valueArr.push(value)
 }
-console.log(minValue)
+//console.log(valueArr)
+//console.log(minValue)
+let min=valueArr[0]
+for(const v of valueArr){
+    if(v < min) min =v
+}
+console.log(min)
