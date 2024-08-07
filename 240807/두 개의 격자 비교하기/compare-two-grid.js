9d58c7a26ade54510cfbe800e30b98bd83f6c237
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-08-07 19:58:55 +0900

    Title: 두 개의 격자 비교하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240807/두 개의 격자 비교하기/compare-two-grid.js
+++ 240807/두 개의 격자 비교하기/compare-two-grid.js
@@ -15,9 +15,9 @@ for(let i = 0; i < n; i++){
     arr2[i] = r[i+4].split(" ").map(Number)
 }
 //console.log(arr1,arr2)
-let arr3=Array(n).fill(0).map(()=>Array(m).fill(0))
+let arr3=Array(n).fill(0).map(()=>Array(n).fill(0))
 for(let i = 0; i < n; i++){
-    for(let j = 0; j < m ; j++){
+    for(let j = 0; j < n ; j++){
           if(arr1[i][j] == arr2[i][j]){
            arr3[i][j] =0
           } 
@@ -26,7 +26,7 @@ for(let i = 0; i < n; i++){
      }
   } //console.log(arr3)
 
-    console.log(arr3[0].join(" "))
+  console.log(arr3[0].join(" "))
   console.log(arr3[1].join(" "))
   console.log(arr3[2].join(" "))
   console.log(arr3[3].join(" "))
