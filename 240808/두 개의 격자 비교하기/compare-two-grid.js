ccba0b8b428bd8c99631aa6b81d034c109490636
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-08-08 18:37:57 +0900

    Title: 두 개의 격자 비교하기, Time: 181ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240808/두 개의 격자 비교하기/compare-two-grid.js
@@ -0,0 +1,37 @@
+const fs= require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n")
+let [a,...r]=input
+//console.log(a)
+let num =  a.split(" ").map(Number)
+let [n,m] = num
+//console.log(r)
+let arr1 = Array(n).fill(0).map(()=>Array(m).fill(0))
+let arr2 = Array(n).fill(0).map(()=>Array(m).fill(0))
+
+for(let i = 0; i < n; i++){
+    arr1[i] = r[i].split(" ").map(Number)
+}
+for(let i = 0; i < n; i++){
+     arr2[i] = r[i+n].split(" ").map(Number) //여기 런타임 에러 뜨는데 왜...
+}
+
+//console.log(arr2)
+let arr3=Array(n).fill(0).map(()=>Array(m).fill(0))
+for(let i = 0; i < n; i++){
+    for(let j = 0; j < m ; j++){
+          if(arr1[i][j] == arr2[i][j]){
+           arr3[i][j] =0
+          } 
+          else arr3[i][j] =1
+     }
+  } //console.log(arr3)
+
+
+
+for(let arr of arr3){
+    let str="";
+    for(let a of arr){
+        str += a + " ";
+    }
+    console.log(str)
+}
