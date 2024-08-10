b0538f6afa8d6ae81a7d27abfbca3615bb520db9
ggang89 <135608183+ggang89@users.noreply.github.com>
2024-08-10 11:08:34 +0900

    Title: 아래로 사각형 채우기, Time: 171ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240810/아래로 사각형 채우기/filling-rectangle-with-downflow.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+
+let arr2d = Array(n).fill(0).map(()=>Array(n).fill(0));
+
+
+for(let i = 0; i < n; i++){
+    let num = 1+i
+    for(let j = 0; j < n; j++){
+        arr2d[i][j] = num
+        num +=5;
+    }
+
+}
+
+for(let arr of arr2d){
+    let str="";
+    for(let a of arr){
+        str += a+" "
+    }
+    console.log(str)
+}
