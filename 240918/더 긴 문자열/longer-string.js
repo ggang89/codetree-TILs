2924298f71386e432fdb50081ef53aec2ae88193
seonyoung Park <135608183+ggang89@users.noreply.github.com>
2024-09-18 22:36:37 +0900

    Title: 더 긴 문자열, Time: 259ms, Memory: 14MB, Status: Passed - CodeTree

--- 240918/더 긴 문자열/longer-string.js
+++ 240918/더 긴 문자열/longer-string.js
@@ -2,10 +2,10 @@ const fs=require("fs");
 let input=fs.readFileSync(0).toString().trim().split(" ");
 let [a,b] =input;
 //console.log(a,b)
-if(a.length>b.length){
+if(a.length > b.length){
     console.log(a,a.length)
-}else if(a.length<b.length){
+}else if(a.length < b.length){
     console.log(b,b.length);
 }else{
-    console.log(same)
+    console.log("same");
 }
