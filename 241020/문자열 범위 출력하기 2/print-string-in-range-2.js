85b22fbab69a6f05784b12f5b9d2893ff486c679
seonyoung Park <135608183+ggang89@users.noreply.github.com>
2024-10-20 22:09:41 +0900

    Title: 문자열 범위 출력하기 2, Time: 182ms, Memory: 14MB, Status: Passed - CodeTree

--- 241020/문자열 범위 출력하기 2/print-string-in-range-2.js
+++ 241020/문자열 범위 출력하기 2/print-string-in-range-2.js
@@ -1,15 +1,22 @@
+// 변수 선언 및 입력
 const fs = require("fs");
-
 let input = fs.readFileSync(0).toString().trim().split("\n");
-let n = Number(input[1]);
+
 let str = input[0];
-let backstr="";
-let cnt=0;
-//console.log(str)
-for(let i = str.length-1; i >= str.length - n; i--){
-    if(cnt >= n) break;
-    backstr +=str[i];
-    cnt ++;
-    
+let a = input[1];
+
+// 문자열의 길이를 구합니다.
+// cnt : 지금까지 출력한 문자의 개수
+let len = str.length;
+let cnt = 0;
+
+// 문자열의 맨 뒤에서부터 주어진 개수만큼 출력합니다.
+let res = "";
+for(let i = len - 1; i >= 0; i--) {
+    // 주어진 개수만큼 출력했을 경우 for문을 나갑니다.
+    if(cnt >= a) break;
+    res += str[i];
+    cnt++;
 }
-console.log(backstr)
+
+console.log(res);
