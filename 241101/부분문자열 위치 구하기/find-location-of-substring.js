6eb2569b4389b35922d2b638c456943f2d363dec
seonyoung Park <135608183+ggang89@users.noreply.github.com>
2024-11-01 17:27:20 +0900

    Title: 부분문자열 위치 구하기, Time: 191ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 241101/부분문자열 위치 구하기/find-location-of-substring.js
+++ 241101/부분문자열 위치 구하기/find-location-of-substring.js
@@ -4,7 +4,10 @@ let input = fs.readFileSync(0).toString().trim().split("\n");
 
 let [str,targetStr] = input;
 let a=targetStr.length//2
-for(let i=0; i<str.length-1;i++){
-    if(str.slice(i,i+a)===targetStr)console.log(i)
-       
-}
+let result=""
+for(let i=0; i<str.length;i++){
+    if(str.slice(i,i+a)===targetStr){
+       result =i
+       break;
+    }
+}console.log(result)
